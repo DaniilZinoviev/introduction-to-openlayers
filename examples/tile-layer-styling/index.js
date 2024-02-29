5dfb9926c1f741d47a34fb6f4bfffa1adeff8e24
DaniilZinoviev <danzino21@gmail.com>
2024-02-29 07:42:22 +0300

    feat: add more tilelayer styling

--- examples/tile-layer-styling/index.js
+++ examples/tile-layer-styling/index.js
@@ -17,18 +17,24 @@ const map = new Map({
 });
 
 const layer = new TileLayer({
+  // Выбираем источник для тайлов
   source: new OGCMapTile({
     url: "https://maps.gnosis.earth/ogcapi/collections/blueMarble/map/tiles/WebMercatorQuad",
   }),
-  // Мы можем так же сделать ограничение и по разрешению карты (или масштабу)
+  // Можем сделать прозрачным
+  opacity: 0.9,
+  // Можем скрывать слой, не убирая его из карты (и теряя тем самым настройки)
+  visible: true,
+  // Можем менять наложение слоёв друг на друга
+  zIndex: 5,
+  // Можем ограничить слой определённой областью
+  extent: [6717612.447527122, 583571.8523972307, 10985130.57817296, 3294022.5569966147],
+  // Можем добавить CSS-стилей
+  className: "favorite-layer",
+  // Можем показывать слой лишь в заданных границах зума
+  maxZoom: 7,
+  // Можем так же показывать слой лишь на определённых разрешениях (масштабе) карты
   // minResolution: 10000,
   // maxResolution: 100000,
-  maxZoom: 7,
-  opacity: 0.9,
-  visible: true,
-  zIndex: 5,
-  extent: [6717612.447527122, 583571.8523972307, 10985130.57817296, 3294022.5569966147],
-  className: "favorite-layer",
 });
-
 map.addLayer(layer);
